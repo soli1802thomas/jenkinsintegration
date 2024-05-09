0e03dbc5ee95a7160f885ece13b4675abd1f532e
solthoma <solthoma@WKWIN6562935.global.publicisgroupe.net>
2024-05-09 12:05:44 +0530

    test

---
+++ jenkinspackage/src/main/java/jenkinspackage/Test.java
@@ -0,0 +1,13 @@
+package jenkinspackage;
+
+public class Test {
+	static String s="Testing to check jenkins";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(s);
+		
+
+	}
+
+}
